c76175f8b4893b62214985793cbd8bd38fc6d01d
Alex-buttielie <buttielle3@gmail.com>
2020-12-03 20:55:48 -0300

    [006-termino de arrumar servidor]

--- portifolio/js/config/routeConfig.js
+++ portifolio/js/config/routeConfig.js
@@ -1,6 +1,14 @@
+
 angular.module("projetoPortifolio").config(function ($routeProvider) {
 	$routeProvider.when("/relatos", {
-		templateUrl: "view/landing-page.html"
-
+		templateUrl: "view/landing-page.html",
+		controller: "portifolioController",
+		resolve: {
+			relatos: function (relatosAPI) {
+				return relatosAPI.getRelatos();
+			},
+		}
 	});
+	
+	$routeProvider.otherwise({redirectTo: "/relatos"});
 });
